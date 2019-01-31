92cf36c831120e7ba5782c0907de1024435eca45
spnova12 <spnova12@gmail.com>
2019-01-31 21:42:12 +0900

    데이터 일괄 생성 스크립트 수정

--- data_makers/big_img_splitter.py
+++ data_makers/big_img_splitter.py
@@ -1,55 +1,69 @@
-# srgb 이미지가 너무 크니까 좀 작게 쪼개주자.
+# challenge 에서 제공받은 srgb 이미지가 너무 크니까 좀 작게 쪼개주자.
 # 일단 겹치지 않게 w, h 각각 10개씩으로 쪼개주었다.
+
+# real 이미지들(challenge 에서 제공받은 데이터 셋) 과
+# 그 ground truth 에 합성노이즈를 추가해준 이미들이 들어있는 폴더 전체에 대해 스크립트가 작동한다.
+
 
 import os
 from os import listdir
 from os.path import join
 from utils.utils import load_BGR, make_dirs
 import cv2
+import tqdm
 
-img_folder_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/Noisy/'
-img_dirs = [join(img_folder_dir, x) for x in sorted(listdir(img_folder_dir))]
+real_origin_folder_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_origin'
 
-img_folder_dir_new = make_dirs('/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/Noisy_split/')
+real_origin_basename = os.path.dirname(real_origin_folder_dir)
+real_splited_folder_dir = real_origin_basename + '/real_splited_none_overlaped/'
 
 # 가로세로 열등분 하자.
 patch_h_count = 10
 patch_w_count = 10
 
-patch_count = 0
+big_img_folder_dirs = [join(real_origin_folder_dir, x) for x in sorted(listdir(real_origin_folder_dir))]
 
-for i, img_dir in enumerate(img_dirs):
-    basename = os.path.basename(img_dir).split('.')[0]
+for big_img_folder_dir in big_img_folder_dirs:
 
-    img = load_BGR(img_dir)
-    print(i, img.shape)
+    img_dirs = [join(big_img_folder_dir, x) for x in sorted(listdir(big_img_folder_dir))]
+    folder_basename = os.path.basename(big_img_folder_dir)
 
-    h = img.shape[0]
-    w = img.shape[1]
+    img_folder_dir_new = make_dirs(real_splited_folder_dir + folder_basename + '/')
 
-    patch_h = h // patch_h_count
-    patch_w = w // patch_w_count
+    patch_count = 0
 
-    for i_h in range(patch_h_count):
-        for i_w in range(patch_w_count):
-            top = patch_h * i_h
-            bottom = top + patch_h
+    for i, img_dir in enumerate(tqdm.tqdm(img_dirs)):
+        basename = os.path.basename(img_dir).split('.')[0]
 
-            if i_h == patch_h_count - 1:
-                bottom = h
+        img = load_BGR(img_dir)
+        # print(i, img.shape)
 
-            left = patch_w * i_w
-            right = left + patch_w
+        h = img.shape[0]
+        w = img.shape[1]
 
-            if i_w == patch_w_count - 1:
-                right = w
+        patch_h = h // patch_h_count
+        patch_w = w // patch_w_count
 
-            img_patch = img[top: bottom, left: right]
+        for i_h in range(patch_h_count):
+            for i_w in range(patch_w_count):
+                top = patch_h * i_h
+                bottom = top + patch_h
 
-            img_patch_name = img_folder_dir_new + str(basename) + '_split_' + str(i_h) + '_' + str(i_w) + '.png'
-            cv2.imwrite(img_patch_name, img_patch)
+                if i_h == patch_h_count - 1:
+                    bottom = h
 
-            patch_count += 1
+                left = patch_w * i_w
+                right = left + patch_w
 
-    print(patch_count)
+                if i_w == patch_w_count - 1:
+                    right = w
+
+                img_patch = img[top: bottom, left: right]
+
+                img_patch_name = img_folder_dir_new + str(basename) + '_' + str(i_h) + '_' + str(i_w) + '.PNG'
+                cv2.imwrite(img_patch_name, img_patch)
+
+                patch_count += 1
+
+        # print(patch_count)
 
--- data_makers/validation_picker.py
+++ data_makers/validation_picker.py
@@ -8,42 +8,76 @@ from random import *
 import shutil
 
 
+train_dirs = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_splited_none_overlaped'
 
-# 학습 데이터셋의 주소
-train_gt_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_splited_Non-overlaped/GroundTruth'
-train_noisy_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_splited_Non-overlaped/Noisy'
+validation_dirs = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/validation'
 
-# 새로 만들 Validation 이미지들의 저장될 주소.
-validation_gt_dir = make_dirs('/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/validation/GroundTruth')
-validation_noisy_dir = make_dirs('/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/validation/Noisy')
 
-train_gt_img_dirs = [join(train_gt_dir, x) for x in sorted(listdir(train_gt_dir))]
-train_noisy_img_dirs = [join(train_noisy_dir, x) for x in sorted(listdir(train_noisy_dir))]
+# 폴더 안의 이미지 수 확인을 위해 임의의 폴더의 이미지 개수 확인.
+train_folder_dirs = [join(train_dirs, x) for x in sorted(listdir(train_dirs))]
+temp = [join(train_folder_dirs[0], x) for x in sorted(listdir(train_folder_dirs[0]))]
+total_img_count = len(temp)
+print(total_img_count)
 
-# 생성할 validation 이미지들의 개수
-validation_img_count = 50
-
+# 0 부터 이미지의 최대 개수 사이의 숫자 중 임의의 숫자 100장 뽑음.
 picked_img_from_trainset = set()
+while len(picked_img_from_trainset) < 100:
+    picked_img_from_trainset.add(randint(0, total_img_count-1))
 
-while len(picked_img_from_trainset) < 50:
-    picked_img_from_trainset.add(randint(0, len(train_gt_img_dirs)-1))
+for train_folder_dir in train_folder_dirs:
+    print('-----------------------------------')
+    folder_basename = os.path.basename(train_folder_dir)
+    img_dirs = [join(train_folder_dir, x) for x in sorted(listdir(train_folder_dir))]
 
-print(picked_img_from_trainset)
+    img_folder_dir_new = make_dirs(validation_dirs + '/' + folder_basename)
 
-for i in picked_img_from_trainset:
-    # gt 옮겨주기.
-    fullname = train_gt_img_dirs[i]
-    basename = os.path.basename(train_gt_img_dirs[i])
-    changedname = validation_gt_dir + '/' + basename
-    shutil.move(fullname, changedname)
-
-    # noisy 옮겨주기
-    fullname = train_noisy_img_dirs[i]
-    basename = os.path.basename(train_noisy_img_dirs[i])
-    changedname = validation_noisy_dir + '/' + basename
-    shutil.move(fullname, changedname)
+    for i in picked_img_from_trainset:
+        fullname = img_dirs[i]
+        basename = os.path.basename(img_dirs[i])
+        changedname = img_folder_dir_new + '/' + basename
+        print(fullname)
+        print(changedname)
+        shutil.move(fullname, changedname)
 
 
+
+
+
+#
+# # 학습 데이터셋의 주소
+# train_gt_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_splited_Non-overlaped/GroundTruth'
+# train_noisy_dir = '/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/train/real_splited_Non-overlaped/Noisy'
+#
+# # 새로 만들 Validation 이미지들의 저장될 주소.
+# validation_gt_dir = make_dirs('/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/validation/GroundTruth')
+# validation_noisy_dir = make_dirs('/home/lab/works/datasets/ssd2/NTIRE_challenge_DB/sRGB/validation/Noisy')
+#
+# train_gt_img_dirs = [join(train_gt_dir, x) for x in sorted(listdir(train_gt_dir))]
+# train_noisy_img_dirs = [join(train_noisy_dir, x) for x in sorted(listdir(train_noisy_dir))]
+#
+# # 생성할 validation 이미지들의 개수
+#
+# picked_img_from_trainset = set()
+#
+# while len(picked_img_from_trainset) < 50:
+#     picked_img_from_trainset.add(randint(0, len(train_gt_img_dirs)-1))
+#
+# print(picked_img_from_trainset)
+#
+# for i in picked_img_from_trainset:
+#     # gt 옮겨주기.
+#     fullname = train_gt_img_dirs[i]
+#     basename = os.path.basename(train_gt_img_dirs[i])
+#     changedname = validation_gt_dir + '/' + basename
+#     shutil.move(fullname, changedname)
+#
+#     # noisy 옮겨주기
+#     fullname = train_noisy_img_dirs[i]
+#     basename = os.path.basename(train_noisy_img_dirs[i])
+#     changedname = validation_noisy_dir + '/' + basename
+#     shutil.move(fullname, changedname)
+#
+#
 
 
 
