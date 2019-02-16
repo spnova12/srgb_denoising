7c55b84c7ea65ab02df26da4c420e384a38bf92e
spnova12 <spnova12@gmail.com>
2019-02-16 11:18:33 +0900

    .

--- models/hevcNet_espcn.py
+++ models/hevcNet_espcn.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from utils.transforms import *
 from models.subNets import *
+import math
 
 
 class hevcnet_espcn(nn.Module):
@@ -13,10 +14,10 @@ class hevcnet_espcn(nn.Module):
         self.layer2 = nn.ReLU()
         self.layer3 = nn.Conv2d(64, 64,  kernel_size=4, stride=2, padding=1)
 
-        #self.layer4 = ResidualBlocks(64, 15)
         self.layer4 = RDB_Blocks(64, 16)
 
         #self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer5 = nn.Conv2d(64, 4 * 64, kernel_size=3, stride=1, padding=1)
         self.layer7 = nn.PixelShuffle(2)
 
         self.layer8 = nn.ReLU()
@@ -27,6 +28,8 @@ class hevcnet_espcn(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
+
+        out = self.layer5(out)
 
         out = self.layer7(out)
         out = self.layer8(out)
