0f95ba135dc5aebe3136c017423e7eae35b35785
cjr <wjdwofus1004@gmail.com>
2019-02-26 19:25:37 +0900

    cbam cases

--- models/hevcNet.py
+++ models/hevcNet.py
@@ -157,6 +157,211 @@ class Generator_one2many_RDB_cbam_ver3(nn.Module):
 
         # global residual 구조
         return out + x
+
+class Generator_one2many_RDB_cbam_ver4(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver4, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        out = self.layer4(out)
+
+        out = self.layer7(out)
+        out = self.cbam(out)
+
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+class Generator_one2many_RDB_cbam_ver5(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver5, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.cbam(out)
+
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+class Generator_one2many_RDB_cbam_ver6(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver6, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.cbam(out)
+
+        out = self.layer4(out)
+
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+class Generator_one2many_RDB_cbam_ver7(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver7, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        out = self.cbam(out)
+        out = self.layer4(out)
+
+        out = self.layer7(out)
+        out = self.cbam(out)
+
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+class Generator_one2many_RDB_cbam_ver8(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver8, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+
+        out = self.layer3(out)
+        out = self.cbam(out)
+
+        out = self.layer4(out)
+        out = self.cbam(out)
+
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+class Generator_one2many_RDB_cbam_ver9(nn.Module):
+    def __init__(self, input_channel):
+        super(Generator_one2many_RDB_cbam_ver9, self).__init__()
+
+        self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.ReLU()
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1)
+
+        # self.layer4 = ResidualBlocks(64, 15)
+        self.layer4 = RDB_Blocks(64, 16)
+
+        self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
+        self.layer8 = nn.ReLU()
+        self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
+
+        self.cbam = CBAM(64, 16)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.cbam(out)
+
+        out = self.layer3(out)
+        out = self.cbam(out)
+
+        out = self.layer4(out)
+        out = self.cbam(out)
+
+        out = self.layer7(out)
+        out = self.cbam(out)
+
+        out = self.layer8(out)
+        out = self.layer9(out)
+
+        # global residual 구조
+        return out + x
+
+
+
     # input x
     # torch.Size([1, 3, 298, 530])
     # layer1
