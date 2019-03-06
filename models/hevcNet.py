ac011dda2ec91242bcf33aaaaf623dae65f42d71
cjr <wjdwofus1004@gmail.com>
2019-03-06 21:21:14 +0900

    no spatial code to gd rir cbam3 non recursive

--- models/hevcNet.py
+++ models/hevcNet.py
@@ -918,6 +918,7 @@ class Generator_one2many_gd_rir_cbam3_non_recursive_relu(nn.Module):
 
         self.numforrg = numforrg  # num of rdb units in one residual group
         self.numofrdb = numofrdb  # num of all rdb units
+        self.nospatial = True
 
         self.layer1 = nn.Conv2d(input_channel, 64, kernel_size=3, stride=1, padding=1)
         self.layer2 = nn.ReLU()
@@ -950,8 +951,8 @@ class Generator_one2many_gd_rir_cbam3_non_recursive_relu(nn.Module):
         self.layer7 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
         self.layer8 = nn.ReLU()
         self.layer9 = nn.Conv2d(64, input_channel, kernel_size=3, stride=1, padding=1)
-        self.cbam1 = CBAM(64, 16, no_spatial=True)
-        self.cbam2 = CBAM(64, 16, no_spatial=True)
+        self.cbam1 = CBAM(64, 16, no_spatial=self.nospatial)
+        self.cbam2 = CBAM(64, 16, no_spatial=self.nospatial)
 
     def forward(self, x):
         out = self.layer1(x)
